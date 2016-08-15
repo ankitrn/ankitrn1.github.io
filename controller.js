8a753c28ffe7d4b48582f085e049fcd7be0cc1b7
Ankit <ankitnatekar@gmail.com>
2016-08-15 17:15:46 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
-app.controller('controller', ['$scope','fetch', function($scope,fetch) {
-    fetch.success(function(data) {
-
+app.controller('controller', function($scope, $http) {
+    $http.get('data.json').success(function(data) {
         $scope.teamData = data;
+    }).error(function(data) {
+        console.log("Team not found. Sorry!");
     });
-}]);
+});
