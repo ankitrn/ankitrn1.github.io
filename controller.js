1253335c9cefa1d1f2e7ca3de3a7ce74e638ddc6
Ankit <ankitnatekar@gmail.com>
2016-08-15 14:36:44 +0530

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,10 +1,7 @@
-app.controller('controller','teams' ,function($scope,$http){
-       $http.get('data.json').success(function(data){
-
-       		$scope.teamData = data;
-
-       }).error(function(data){
-
-            console.log("Team not found. Sorry!");
-       });
-   });
+app.controller('controller', function($scope, $http) {
+    $http.get('data.json').success(function(data) {
+        $scope.teamData = data;
+    }).error(function(data) {
+        console.log("Team not found. Sorry!");
+    });
+});
